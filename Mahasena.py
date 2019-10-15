8feb8000266790c538901a790c8278186a0b843d
Shria Devarakonda <54900370+shria-devarakonda@users.noreply.github.com>
2019-10-15 05:35:40 +0530

    Create Mahasena.py

---
+++ Mahasena.py
@@ -0,0 +1,15 @@
+# cook your dish here
+
+n = int(input())
+listy = list(map(int, input().split()))
+count = 0
+uncount = 0
+for i in range(n):
+    if listy[i] % 2 == 0:
+        count = count + 1
+    else:
+        uncount = uncount + 1
+if count>uncount:
+    print("READY FOR BATTLE")
+else:
+    print("NOT READY")
